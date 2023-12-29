ad468040216ef9c010ae8fffedc7c29d2c45858a
katitanguay <146486992+katitanguay@users.noreply.github.com>
2023-12-29 13:43:42 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ import matplotlib.pyplot as plt
 
 st.title('NoramlizeMe HGS-OvCa in HM450k')
 
-datadfdf.to_csv('HM450k_EpicGeneSum.tsv.gz', sep = '\t', compression = 'gzip')
+df = st.file_uploader("HM450k_EpicGeneSum.tsv.gz")
+st.write(df)
