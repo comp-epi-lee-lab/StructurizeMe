269e59e8d38aadbdf30316bc10cab10e2b1dc7d3
katitanguay <146486992+katitanguay@users.noreply.github.com>
2024-10-17 14:21:43 -0400

    app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ def load_data():
     return {
         'BRCA': {
             'cancer': pd.read_csv('data/BRCA-Cancer.tsv.gz', sep='\t', compression='gzip'),
-            'normal': pd.read_csv('data/BRCA-Normal.tsv.gz', sep='\t', compression='gzip'),
-            'diff': pd.read_csv('data/BRCA-Differences.tsv.gz', sep='\t', compression='gzip')
+            'normal': pd.read_csv('data/BRCA-Normal.tsv.gz', sep='\t', compression='gzip')
+            #'diff': pd.read_csv('data/BRCA-Differences.tsv.gz', sep='\t', compression='gzip')
         },
         'COAD': {
             'cancer': pd.read_csv('data/COAD-Cancer.tsv.gz', sep='\t', compression='gzip'),
-            'normal': pd.read_csv('data/COAD-Normal.tsv.gz', sep='\t', compression='gzip'),
-            'diff': pd.read_csv('data/COAD-Differences.tsv.gz', sep='\t', compression='gzip')
+            'normal': pd.read_csv('data/COAD-Normal.tsv.gz', sep='\t', compression='gzip')
+            #'diff': pd.read_csv('data/COAD-Differences.tsv.gz', sep='\t', compression='gzip')
         },
         'OV': {
             'cancer': pd.read_csv('data/HM450k_EpicGeneSum1.tsv.gz', sep='\t', compression='gzip'),
