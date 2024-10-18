8ae76f8095d922a3b775f1a9f2f70543acdfaa3d
katitanguay <146486992+katitanguay@users.noreply.github.com>
2024-10-18 10:46:45 -0400

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -10,12 +10,10 @@ def load_data():
         'BRCA': {
             'cancer': pd.read_csv('data/BRCA-Cancer.tsv.gz', sep='\t', compression='gzip'),
             'normal': pd.read_csv('data/BRCA-Normal.tsv.gz', sep='\t', compression='gzip')
-            #'diff': pd.read_csv('data/BRCA-Differences.tsv.gz', sep='\t', compression='gzip')
         },
         'COAD': {
             'cancer': pd.read_csv('data/COAD-Cancer.tsv.gz', sep='\t', compression='gzip'),
             'normal': pd.read_csv('data/COAD-Normal.tsv.gz', sep='\t', compression='gzip')
-            #'diff': pd.read_csv('data/COAD-Differences.tsv.gz', sep='\t', compression='gzip')
         },
         'OV': {
             'cancer': pd.read_csv('data/HM450k_EpicGeneSum1.tsv.gz', sep='\t', compression='gzip'),
@@ -23,26 +21,18 @@ def load_data():
             'diff': pd.read_csv('data/Differences_HM450EPICv1.gz', sep='\t', compression='gzip')
         }
     }
-    
-data = load_data()
 
 # Function to plot heatmaps
 def plot_heatmap(data, title, yticklabels=None):
     fig, ax = plt.subplots(figsize=(15, 5))
 
     if not data.empty:
-        # Debugging output to check yticklabels
-        st.write(f"yticklabels before: {yticklabels}")
-
         if yticklabels is None:
             if isinstance(data.index, pd.MultiIndex):
                 yticklabels = data.index.get_level_values('Gene')
             else:
-                yticklabels = data.index  # Fallback if not a MultiIndex
-         
-        st.write(f"yticklabels after: {yticklabels}")  # Debugging output
-                
-    if not data.empty:
+                yticklabels = data.index
+
         sns.heatmap(data, cmap="vlag", annot=False, linewidths=.5,
                     yticklabels=yticklabels, linecolor='grey', annot_kws={"size": 12}, cbar_kws={'ticks': [0.0, 0.5, 1.0]}, vmin=0, vmax=1)
         plt.title(title, fontfamily='sans-serif')
@@ -51,14 +41,6 @@ def plot_heatmap(data, title, yticklabels=None):
         plt.tick_params(left=False, bottom=False)
         ax.spines['bottom'].set_visible(True)
         ax.spines['right'].set_visible(True)
-        ax.spines['bottom'].set_linewidth(.5)
-        ax.spines['right'].set_linewidth(.5)
-        spine_color = 'grey'
-        ax.spines['bottom'].set_color(spine_color)
-        ax.spines['right'].set_color(spine_color)
-        ax.patch.set_edgecolor('lightgrey')
-        ax.patch.set_hatch('///')
-        st.write("Selected Genes in EPIC850k:")
         st.pyplot(fig)
     else:
         st.write("No data available for this selection.")
@@ -82,57 +64,55 @@ if selected_cancers:
         st.write(f'Selected Cancer Types: {", ".join(selected_cancers)}')
         st.write(f'Selected Gene Names: {", ".join(selected_genes)}')
 
-# Iterate through selected cancers and plot heatmaps
-for cancer in selected_cancers:
-    # Data for Cancer
-    selected_cancer_data = data_dict[cancer]['cancer'][
-        data_dict[cancer]['cancer']['Gene'].isin(selected_genes) |
-        data_dict[cancer]['cancer'].get('alias_symbol', pd.Series([])).isin(selected_genes)
-    ]
-    
-    if not selected_cancer_data.empty:
-        selected_cancer_data.set_index(['Gene', 'alias_symbol'], inplace=True)
-        plot_heatmap(selected_cancer_data, f"{cancer} Cancer")
-    else:
-        st.write(f"No data available for {cancer} Cancer.")
+        # Iterate through selected cancers and plot heatmaps
+        for cancer in selected_cancers:
+            # Data for Cancer
+            selected_cancer_data = data_dict[cancer]['cancer'][
+                data_dict[cancer]['cancer']['Gene'].isin(selected_genes) |
+                data_dict[cancer]['cancer'].get('alias_symbol', pd.Series([])).isin(selected_genes)
+            ]
 
-    # Data for Normal
-    selected_normal_data = data_dict[cancer]['normal'][
-        data_dict[cancer]['normal']['Gene'].isin(selected_genes) |
-        data_dict[cancer]['normal'].get('alias_symbol', pd.Series([])).isin(selected_genes)
-    ]
-    
-    if not selected_normal_data.empty:
-        selected_normal_data.set_index(['Gene', 'alias_symbol'], inplace=True)
-        plot_heatmap(selected_normal_data, f"{cancer} Normal")
-    else:
-        st.write(f"No data available for {cancer} Normal.")
-
-    # Data for Differences
-    if 'diff' in data_dict[cancer]:
-        selected_diff_data = data_dict[cancer]['diff'][
-            data_dict[cancer]['diff']['Gene'].isin(selected_genes) |
-            data_dict[cancer]['diff'].get('alias_symbol', pd.Series([])).isin(selected_genes)
-        ]
-        
-        if not selected_diff_data.empty:
-            selected_diff_data.set_index(['Gene', 'alias_symbol'], inplace=True)
-            plot_heatmap(selected_diff_data, f"{cancer} Differences")
-        else:
-            st.write(f"No data available for {cancer} Differences.")
-
-            # Create values_data by filtering columns in selected_diff_data
-            values_data = selected_cancer_data.loc[:, selected_diff_data.columns]
-
-            # Check if values_data has non-null values
-            values_data = values_data[values_data.notna().any(axis=1)]
-
-            # Plotting values_data heatmap
-            if not values_data.empty:
-                plot_heatmap(values_data, f"{cancer} Selected Genes with Values from Differences")
+            if not selected_cancer_data.empty:
+                selected_cancer_data.set_index(['Gene', 'alias_symbol'], inplace=True)
+                plot_heatmap(selected_cancer_data, f"{cancer} Cancer")
             else:
-                st.write("No data available in values_data for the selected differences.")
-        else:
-            st.write(f"No difference data available for {cancer}.")
-    else:
-        st.write(f"No data available for {cancer} Cancer.")
+                st.write(f"No data available for {cancer} Cancer.")
+
+            # Data for Normal
+            selected_normal_data = data_dict[cancer]['normal'][
+                data_dict[cancer]['normal']['Gene'].isin(selected_genes) |
+                data_dict[cancer]['normal'].get('alias_symbol', pd.Series([])).isin(selected_genes)
+            ]
+
+            if not selected_normal_data.empty:
+                selected_normal_data.set_index(['Gene', 'alias_symbol'], inplace=True)
+                plot_heatmap(selected_normal_data, f"{cancer} Normal")
+            else:
+                st.write(f"No data available for {cancer} Normal.")
+
+            # Data for Differences
+            if 'diff' in data_dict[cancer]:
+                selected_diff_data = data_dict[cancer]['diff'][
+                    data_dict[cancer]['diff']['Gene'].isin(selected_genes) |
+                    data_dict[cancer]['diff'].get('alias_symbol', pd.Series([])).isin(selected_genes)
+                ]
+
+                if not selected_diff_data.empty:
+                    selected_diff_data.set_index(['Gene', 'alias_symbol'], inplace=True)
+                    plot_heatmap(selected_diff_data, f"{cancer} Differences")
+
+                    # Create values_data from selected_diff_data
+                    values_data = selected_cancer_data.loc[:, selected_diff_data.columns]
+                    values_data = values_data[values_data.notna().any(axis=1)]  # Keep only rows with non-null values
+
+                    if not values_data.empty:
+                        plot_heatmap(values_data, f"{cancer} Selected Genes with Values from Differences")
+                    else:
+                        st.write("No data available in values_data for the selected differences.")
+                else:
+                    st.write(f"No difference data available for {cancer}.")
+            else:
+                st.write(f"No difference data available for {cancer}.")
+else:
+    st.write("Please select cancer types.")
+
