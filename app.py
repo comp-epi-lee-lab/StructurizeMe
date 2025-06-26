50cb3fb1f15015dc96d1a592f1c9ecd4e859e12e
katitanguay <146486992+katitanguay@users.noreply.github.com>
2025-06-26 12:01:59 -0400

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Function to load data for each cancer type
 def load_data():
     return {
         'BRCA': {
-            'cancer': pd.read_csv('data/BRCA-Cancer.tsv.gz', sep='\t', compression='gzip'),
-            'normal': pd.read_csv('data/BRCA-Normal.tsv.gz', sep='\t', compression='gzip')
+            'cancer': pd.read_csv('data/BRCA Files/BRCA-Cancer.tsv.gz', sep='\t', compression='gzip'),
+            'normal': pd.read_csv('data/BRCA Files/BRCA-Normal.tsv.gz', sep='\t', compression='gzip')
+            #'diff': pd.read_csv('data/BRCA-Differences.tsv.gz', sep='\t', compression='gzip')
         },
         'COAD': {
-            'cancer': pd.read_csv('data/COAD-Cancer.tsv.gz', sep='\t', compression='gzip'),
-            'normal': pd.read_csv('data/COAD-Normal.tsv.gz', sep='\t', compression='gzip')
+            'cancer': pd.read_csv('data/COAD Files/COAD-Cancer.tsv.gz', sep='\t', compression='gzip'),
+            'normal': pd.read_csv('data/COAD Files/COAD-Normal.tsv.gz', sep='\t', compression='gzip')
+            #'diff': pd.read_csv('data/COAD-Differences.tsv.gz', sep='\t', compression='gzip')
         },
         'OV': {
-            'cancer': pd.read_csv('data/HM450k_EpicGeneSum1.tsv.gz', sep='\t', compression='gzip'),
-            'normal': pd.read_csv('data/EPIC850k_EpicGeneSum1.tsv.gz', sep='\t', compression='gzip'),
+            'cancer': pd.read_csv('data/OV Files/HM450k_EpicGeneSum1.tsv.gz', sep='\t', compression='gzip'),
+            'normal': pd.read_csv('data/OV Files/EPIC850k_EpicGeneSum1.tsv.gz', sep='\t', compression='gzip'),
             'diff': pd.read_csv('data/Differences_HM450EPICv1.gz', sep='\t', compression='gzip')
         }
     }
-
+    
 data = load_data()
